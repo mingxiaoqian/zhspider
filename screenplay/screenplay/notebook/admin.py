f138b49b2382494a2ae4815548c24377ee40b411
ming_qian <xxjsqm@126.com>
2017-04-30 17:11:10 +0800

    add models : screenplay
    Signed-off-by: ming_qian <xxjsqm@126.com>

--- screenplay/screenplay/notebook/admin.py
+++ screenplay/screenplay/notebook/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import ScreenPlay
+
+class ScreenPlayAdmin(admin.ModelAdmin):
+    list_display = ("case_no", "name", "filling_unit", "author")
+
+admin.site.register(ScreenPlay, ScreenPlayAdmin)
+
--- screenplay/screenplay/notebook/models.py
+++ screenplay/screenplay/notebook/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class ScreenPlay(models.Model):
+    case_no = models.CharField(max_length=64, primary_key=True)
+    name = models.CharField(max_length=64)
+    filling_unit = models.CharField(max_length=128)
+    author = models.CharField(max_length=64)
+    result = models.CharField(max_length=32)
+    region = models.CharField(max_length=32)
+    case_url = models.URLField(max_length=256)
+    path_url = models.URLField(max_length=256)
+    case_time = models.CharField(max_length=32)
+
+
+    def __str__(self):
+        return self.name
