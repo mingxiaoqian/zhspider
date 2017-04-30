9326190f9fc1b2aa9cb9599d4f601da2de0ac986
ming_qian <xxjsqm@126.com>
2017-04-30 17:03:15 +0800

    add templates :base.html/home.htm
    Signed-off-by: ming_qian <xxjsqm@126.com>

--- screenplay/screenplay/settings.py
+++ screenplay/screenplay/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'screenplay.notebook',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'screenplay.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "screenplay/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
